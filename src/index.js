616041ded444602efe3e8375c82776deca378a58
Andrew <andrew-s@comcast.net>
2016-08-21 17:07:22 -0400

    fixed a typo when refactoring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import App from './app/containers/App';
+import App from './app/containers/app/App';
 import configureStore from './app/store/configureStore';
 import {Router, Route, browserHistory} from 'react-router';
 
