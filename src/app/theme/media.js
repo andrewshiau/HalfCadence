dc1fd120eb76032ae0d897cb2a79f81cb6f765fc
Andrew <andrew-s@comcast.net>
2016-09-09 23:14:52 -0400

    fixed media query js computation bug

--- src/app/theme/media.js
+++ src/app/theme/media.js
@@ -1,19 +1,22 @@
+// media queries
+// converts number arguments to rems
+
 export const devices = {
   // mobile that is so small it's probably a vertical screen
   // probably not so aptly named
-  tallmobile: '30rem',
+  tallmobile: 30,
   // normal mobile
-  mobile: '50rem'
+  mobile: 50
 };
 
 export function widerThan(width) {
-  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (min-width: ${width}),` +
-    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (min-width: 2 * ${width}),` +
-    `screen and (min-resolution: 3dppx) and (min-width: 3 * ${width})`;
+  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (min-width: ${width}rem),` +
+    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (min-width: ${2 * width}rem),` +
+    `screen and (min-resolution: 3dppx) and (min-width: ${3 * width}rem)`;
 }
 
 export function narrowerThan(width) {
-  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (max-width: ${width}),` +
-    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (max-width: 2 * ${width}),` +
-    `screen and (min-resolution: 3dppx) and (max-width: 3 * ${width})`;
+  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (max-width: ${width}rem),` +
+    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (max-width: ${2 * width}rem),` +
+    `screen and (min-resolution: 3dppx) and (max-width: ${3 * width}rem)`;
 }
