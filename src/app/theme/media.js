41b80cf095dacfb9bbfbf2db7a78b0d90f134b6d
Andrew <andrew-s@comcast.net>
2016-09-10 18:40:12 -0400

    rewrote media queries without fiddling

--- src/app/theme/media.js
+++ src/app/theme/media.js
@@ -3,24 +3,14 @@
 // so prefixed to not break mobile
 
 export const devices = {
-  tallmobile: 30,
-  mobile: 50
+  tallmobile: 400,
+  mobile: 800
 };
 
 export function widerThan(width) {
-  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (min-width: ${width}rem),` +
-    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (min-width: ${2 * width}rem),` +
-    `screen and (min-resolution: 3dppx) and (min-width: ${3 * width}rem)` +
-    `screen and (-webkit-min-device-pixel-ratio: 1dppx) and (-webkit-max-device-pixel-ratio: 1.9dppx) and (min-width: ${width}rem),` +
-    `screen and (-webkit-min-device-pixel-ratio: 2dppx) and (-webkit-max-device-pixel-ratio: 2.9dppx) and (min-width: ${2 * width}rem),` +
-    `screen and (-webkit-min-device-pixel-ratio: 3dppx) and (min-width: ${3 * width}rem)`;
+  return `@media screen and (min-width: ${width}px)`;
 }
 
 export function narrowerThan(width) {
-  return `@media screen and (min-resolution: 1dppx) and (max-resolution: 1.9dppx) and (max-width: ${width}rem),` +
-    `screen and (min-resolution: 2dppx) and (max-resolution: 2.9dppx) and (max-width: ${2 * width}rem),` +
-    `screen and (min-resolution: 3dppx) and (max-width: ${3 * width}rem)` +
-    `screen and (-webkit-min-device-pixel-ratio: 1dppx) and (-webkit-max-device-pixel-ratio: 1.9dppx) and (max-width: ${width}rem),` +
-    `screen and (-webkit-min-device-pixel-ratio: 2dppx) and (-webkit-max-device-pixel-ratio: 2.9dppx) and (max-width: ${2 * width}rem),` +
-    `screen and (-webkit-min-device-pixel-ratio: 3dppx) and (max-width: ${3 * width}rem)`;
+  return `@media screen and (max-width: ${width}px)`;
 }
